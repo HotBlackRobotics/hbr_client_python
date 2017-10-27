176325d5337ed55f02092868ca8b4cfca8f96c1b
Ludovico O. Russo <ludus.russo@gmail.com>
2017-10-27 12:16:07 +0200

    up setuo

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
 setup(
-  name = 'hbr_client',
-  packages = ['hbr_client'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Python Library to consume HBR Cloud API',
-  author = 'HotBlack Robotics',
-  author_email = 'info@hotblackrobotics.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['HBR', 'logging'], # arbitrary keywords
-  classifiers = [],
+    name = 'hbr_client',
+    packages = ['hbr_client'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Python Library to consume HBR Cloud API',
+    author = 'HotBlack Robotics',
+    author_email = 'info@hotblackrobotics.com',
+    url = 'https://github.com/HotBlackRobotics/hbr_client_python', # use the URL to the github repo
+    download_url = 'https://github.com/HotBlackRobotics/hbr_client_python/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = ['HBR', 'logging'], # arbitrary keywords
+    classifiers = [],
+    install_requires=[
+        'requests'
+    ],
 )
