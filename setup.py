fa195939280b74d892b91a4b1f74627c060fb761
Ludovico O. Russo <ludus.russo@gmail.com>
2017-10-27 11:26:02 +0200

    add conf files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hbr_client',
+  packages = ['hbr_client'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python Library to consume HBR Cloud API',
+  author = 'HotBlack Robotics',
+  author_email = 'info@hotblackrobotics.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['HBR', 'logging'], # arbitrary keywords
+  classifiers = [],
+)
